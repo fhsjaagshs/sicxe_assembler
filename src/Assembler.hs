abb21d310b41d850ec7b5264ed31fe7c353eeb67
Nate Symer <nate@symer.io>
2016-12-09 16:12:03 -0500

    Progress on addressing

--- src/Assembler.hs
+++ src/Assembler.hs
@@ -21,6 +21,7 @@ import Data.Bits
 -- High level assembly logic
 --
 
+-- TODO: fix fallback to format 4 when format 3 cannot handle an instr 
 lineFormat :: Line -> Maybe Int 
 lineFormat (Line _ (Mnemonic _ extended) oprs) = case lookupMnemonic m of
   Nothing -> Nothing
@@ -35,8 +36,8 @@ lineFormat (Line _ (Mnemonic _ extended) oprs) = case lookupMnemonic m of
 sizeofLine :: Line -> Maybe Int
 sizeofLine = lineFormat
 
--- | Assembles a line of SIC/XE ASM as parsed by Parser. Returns a buffer and the next address.
-assembleLine :: Word32 -> [(String, Word32)] -> Line -> Maybe ([Bool], Word32)
+-- | Assembles a line of SIC/XE ASM as parsed by Parser. Returns a list of bytes in Big Endian order and the next address.
+assembleLine :: Word32 -> [(String, Word32)] -> Line -> Maybe ([Word8], Word32)
 assembleLine addr symtab l@(Line _ (Mnemonic m _) oprs) = (,) <$> mb <*> maddr
   where
     mbools = g ((,) <$> lineFormat l <*> sizeofLine l))
@@ -45,12 +46,12 @@ assembleLine addr symtab l@(Line _ (Mnemonic m _) oprs) = (,) <$> mb <*> maddr
     g Nothing                    = mkdirec m oprs
     mkinstr opc 1 _    = Just $ format1 opc
     mkinstr opc 2 oprs = format2 opc
-                         <$> (safeIdx 0 oprs >>= lookupRegister)
-                         <*> (safeIdx 1 oprs >>= lookupRegister)
-    mkinstr opc 3 [a, b] = format3 (getN a) (getI a) (getX a b) <$> getDisp addr symtab a
-    mkinstr opc 3 [a]    = format3 (getN a) (getI a) False      <$> getDisp addr symtab a
-    mkinstr opc 4 [a, b] = format4 (getN a) (getI a) (getX a b) <$> getAddr a
-    mkinstr opc 4 [a]    = format4 (getN a) (getI a) False      <$> getAddr a
+                         <$> (safeIdx 0 oprs >>= simpleToByte)
+                         <*> (safeIdx 1 oprs >>= simpleToByte)
+    mkinstr opc 3 [a, b] = format3 (getN a) (getI a) (getX a b) <$> getAddr symtab a
+    mkinstr opc 3 [a]    = format3 (getN a) (getI a) False      <$> getAddr symtab a
+    mkinstr opc 4 [a, b] = format4 (getN a) (getI a) (getX a b) <$> getAddr symtab a
+    mkinstr opc 4 [a]    = format4 (getN a) (getI a) False      <$> getAddr symtab a
 
     -- TODO: IMPLEMENT DIRECTIVES
     --       1. ones that assemble
@@ -77,11 +78,9 @@ getI a = isType OpImmediate a || isType OpSimple a
 getN :: Operand -> Bool
 getN a = isType OpIndirect a || isType OpSimple a
 
-getDisp :: Word32 -> [(String, Word32)] -> Operand -> Maybe Int
-getDisp addr symtab = const Nothing
-
-getAddr :: Operand >- Maybe Int
-getAddr = const Nothing
+getAddr :: [(String, Word32)] -> Operand -> Maybe Word32
+getAddr symtab (Operand (Left s) _) = Just s
+getAddr symtab (Operand (Right s) _) = lookup symtab s
 
 -- | Turns a data structure representing bits into a list of bits in bit endian order,
 -- ignoring bit ordering.
@@ -93,6 +92,21 @@ toBits x = map (testBit x . uncurry (+) . idx2bitbyte) [0..bitlen - 1]
   where bitlen = finiteBitSize x
         idx2bitbyte idx = (7 - mod ix 8, div idx 8)
 
+-- | Packs a list of bits into a list of bytes
+-- in machine order
+packBits :: [Bool] -> [Word8]
+packBits = f []
+  where
+    f acc [] = acc
+    f acc xs = f (acc ++ [b]) xs'
+      where
+        (bits, xs') = splitAt 8
+        bits' = bits ++ replicate (max (8 - length bits) 0) False
+        indeces = [7,6,5,4,3,2,1,0]
+        b = [bit' i v | v <- bits', i <- indeces]
+    bit' i True = bit i
+    bit' i False = zeroBits
+
 safeIdx :: Int -> [a] -> Maybe a
 safeIdx i xs
   | length xs > i = Just $ xs !! i
@@ -101,10 +115,10 @@ safeIdx i xs
 lookupMnemonic :: String -> Maybe OpDesc
 lookupMnemonic m = find ((==) m . opdescMnemonic) operations
 
-lookupRegister :: Operand -> Maybe Word8
-lookupRegister (Operand (Right ident) OpSimple) = lookup ident registers
-lookupRegister (Operand (Left i) OpSimple) = Just $ fromIntegral i
-lookupRegister _ = Nothing
+simpleToByte :: Operand -> Maybe Word8
+simpleToByte (Operand (Right ident) OpSimple) = lookup ident registers
+simpleToByte (Operand (Left i) OpSimple) = Just $ fromIntegral i
+simpleToByte _ = Nothing
 
 --
 -- Symbol Table
@@ -117,22 +131,39 @@ mkSymbolTable = f 0 []
           Just s -> f (addr + s) (acc + (l, addr)) ls
           Nothing -> Nothing
 
+-- TODO: implement me!!
+getStartAddress :: [(String, Word32)] -> [Line] -> Word32
+getStartAddress _ _ = 0
+
 --
 -- Formatting Functions (low-level assembly logic)
 --
 
-format1 :: Word8 -> [Bool]
-format1 = toBits
+format1 :: Word8 -> [Word8]
+format1 = pure
 
-format2 :: Word8 -> Word8 -> Word8 -> [Bool]
-format2 op rega regb = toBits op ++ toBits regb
-  where regs = ((rega `shiftL` 4) .|. regb)
+format2 :: Word8 -> Word8 -> Word8 -> [Word8]
+format2 op rega regb = [op, shiftL rega 4 .|. regb]
 
-format3 :: Word8 -> Bool -> Bool -> Bool -> Bool -> Bool -> Integer -> [Bool]
-format3 op n i x b p disp = format32DRY op n i x b p ++ (take 12 $ toBits disp) -- Is this going to be the right order?
+-- TODO: implement calculating B and P
+format3 :: Word8 -> Bool -> Bool -> Bool -> Word32 -> Word32 -> [Word8]
+format3 op n i x curaddr memoff
+ | b || p = packBits $ prefix ++ take 12 (toBits disp)
+ | otherwise = format4 op n i x memoff
+  where
+    disp = curaddr - memoff
+    prefix = format34DRY op n i x b p False
+    b = disp >= 0 && disp <= 4095
+    p = disp >= -2048 && disp <= 2047
 
-format4 :: Word8 -> Bool -> Bool -> Bool -> Integer -> [Bool]
-format4 op n i x addr = format34DRY op n i x False False True ++ (take 20 $ toBits addr)
+format4 :: Word8 -> Bool -> Bool -> Bool -> Integer -> [Word8]
+format4 op n i x addr = packBits $ prefix ++ addr'
+  where
+    prefix = format34DRY op n i x False False True
+    addr' = take 20 $ toBits addr
 
 format34DRY :: Word8 -> Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> [Bool]
 format34DRY op n i x b p e = toBits (set (set op 0 i) 1 n) ++ [x, b, p, e]
+  where set v i True = setBit v i
+        set v i False = clearBit v i
+
